fbf004884902190b33186da3ecf7a14b4e86d5e7
Dimid Duchovny <dimidd@NO_SPAM.gmail.com>
2014-04-11 01:47:24 +0300

    search: fix builtin override

--- aima/search.py
+++ aima/search.py
@@ -78,8 +78,8 @@ class Node:
 
     def child_node(self, problem, action):
         "Fig. 3.10"
-        next = problem.result(self.state, action)
-        return Node(next, self, action,
+        next_state = problem.result(self.state, action)
+        return Node(next_state, self, action,
                     problem.path_cost(self.path_cost, self.state, action, next))
 
     def solution(self):
