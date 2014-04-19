36278ca933d9a564d3baf9727ace0e87429154cc
Dimid Duchovny <dimidd@NO_SPAM.gmail.com>
2014-04-20 02:02:44 +0300

    main: bounds

--- smartroom/main.py
+++ smartroom/main.py
@@ -1,39 +1,69 @@
 from aima.search import astar_search as astar
-from smartroom.room_item import ItemDescriptor
-from smartroom.room_item import Bench
 from smartroom.room_state import RoomState
 from smartroom.room_goal import GoalVerifier
 from smartroom.room_problem import RoomProblem
 from smartroom.room_constraint import IsolatedItems
+from smartroom.room_item import *
 
-#TODO: get input from commandline
+class InitialStateInputErr (Exception):
+    def __init__ (self, token):
+        self.token = token
+     
+    def __str__(self):
+        return 'Invalid token:'+self.token   
+
+# TODO: validate input
+def read_descriptors(descs_filename):
+    descs = []
+    with open(descs_filename) as desc_file:
+        for line in desc_file:
+            splitted = line.split()
+            class_name = splitted[0]
+            params = splitted[1]
+            n_items = splitted[2]
+            # TODO: replace eval with something safer
+            item = eval (class_name + params)
+            item_desc = ItemDescriptor(item, int(n_items))
+            descs.append(item_desc)
+    return descs
 
 
-descs_filename="input1.txt"
-initial_filename="initial2.txt"
+def read_initial (initial_filename):
+    initial = []
+    bounds = []
+    # TODO: support also 0,1 instead of True,False
+    with open(initial_filename) as initial_file:
+        try:
+            for line in initial_file:
+                splitted = line.replace('\n','').split(', ')
+                for bool_str in splitted:
+                    initial.append('True' == bool_str.replace('|', ''))
+                bounds = [False] * (len (initial) + 1)
+                bounds [0] = bounds [-1] = True
+                for index,bool_str in enumerate(splitted):
+                    if bool_str in ('False', 'True'):
+                        continue
+                    if '|True' == bool_str:
+                        bounds[index] = True
+                    elif 'True|' == bool_str:
+                        bounds[index+1] = True
+                    else:
+                        raise InitialStateInputErr(bool_str)
+            return initial, bounds
+               
+        except Exception as e:
+            print e
+            
+    
 
-descs = []
-#TODO: validate input
-with open(descs_filename) as desc_file:
-    for line in desc_file:
-        splitted = line.split()
-        class_name = splitted[0]
-        params = splitted[1]
-        n_items = splitted[2]
-        #TODO: replace eval with something safer 
-        item = eval (class_name+params)
-        item_desc = ItemDescriptor(item, int(n_items))
-        descs.append(item_desc)
 
-initial=[]
-#TODO: support also 0,1 instead of True,False
-with open(initial_filename) as initial_file:
-    for line in initial_file:
-        splitted = line.split(', ')
-        for bool_str in splitted:
-            initial.append('True' == bool_str)
+# TODO: get input from commandline
+descs_filename = "input2.txt"
+initial_filename = "bounds.txt"
+descs = read_descriptors(descs_filename)
+initial, bounds = read_initial (initial_filename)
 
-initial_state = RoomState(initial)
+initial_state = RoomState(initial, bounds)
 verf_nocst = GoalVerifier([], descs)
 verf_isolt = GoalVerifier([IsolatedItems()], descs)
 problem = RoomProblem (initial_state, verf_isolt)
