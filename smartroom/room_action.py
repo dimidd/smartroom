3af8cbd8d55e7044c2970790333aaa43edba3f9f
Dimid Duchovny <dimidd@NO_SPAM.gmail.com>
2014-04-22 02:27:52 +0300

    action: pep8 fixes

--- smartroom/room_action.py
+++ smartroom/room_action.py
@@ -7,18 +7,19 @@ Created on 10/04/2014
 @author: dimid
 '''
 
+
 class RoomAction(object):
     '''
-    classdocs
+    Abstract room action
     '''
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
-    def apply(self, state):
+    def apply(self, _orig_state):
         return
-    
+
     @abc.abstractmethod
-    def path_cost(self, c, state1, state2):
+    def path_cost(self, _c, _state1, _state2):
         """Return the cost of a solution path that arrives at state2 from
         state1 via action, assuming cost c to get up to state1. If the problem
         is such that the path doesn't matter, this function will only look at
@@ -26,17 +27,18 @@ class RoomAction(object):
         and action. The default method costs 1 for every step in the path."""
 
         return
-        
+
+
 class FlipChair (RoomAction):
     ''' Flip the @k-th chair
     '''
-    def __init__ (self, k):
+    def __init__(self, k):
         self.index = k
-        
-    def apply (self, orig_state):
+
+    def apply(self, orig_state):
         state = copy.deepcopy(orig_state)
         state.seats[self.index] = not state.seats[self.index]
-        return state;
-    
-    def path_cost(self, c, state1, state2):
+        return state
+
+    def path_cost(self, c, _state1, _state2):
         return c + 1
