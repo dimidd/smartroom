9978b4c9bd304e68f3a594a9c7919b1a3ca67c17
Dimid Duchovny <dimidd@NO_SPAM.gmail.com>
2014-04-22 02:51:03 +0300

    goal: use state.analyze() to count items

--- smartroom/room_goal.py
+++ smartroom/room_goal.py
@@ -12,23 +12,21 @@ class GoalVerifier(object):
 
     def __init__(self, constraints, item_descriptors):
         self.constarints = constraints
-        self.descriptors = item_descriptors              
-        
-    # TODO: for the meantime assume we have just 1x1 items. How to count arbitrary shapes?
-    def count_items(self, state, item):
-        count = 0
-        for s in state.seats:
-            if s:
-                count += 1 
-        return count
-         
-    def is_goal (self, state):
+        self.descriptors = item_descriptors
+
+    # TODO: assume we have just 1xN items. How to count arbitrary shapes?
+    def count_items(self, state, _item):
+        _spaces, shapes, _unsatisfied = state.analyze(self)
+        return len(shapes)
+
+    def is_goal(self, state):
         for c in self.constarints:
             if not c.is_valid(state):
                 return False
-            
-        for d in self.descriptors:
-            if d.count != self.count_items(state, d.item):
+
+        _spaces, _shapes, unsatisfied = state.analyze(self)
+        for us in unsatisfied:
+            if us.count > 0:
                 return False
 
         return True
