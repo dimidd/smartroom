ccaeb1825d3ec6c788fb754d2ffc2342c9624adf
Dimid Duchovny <dimidd@NO_SPAM.gmail.com>
2014-04-22 02:33:09 +0300

    constraint: pep8 fixes

--- smartroom/room_constraint.py
+++ smartroom/room_constraint.py
@@ -8,31 +8,31 @@ import abc
 from _pyio import __metaclass__
 from abc import ABCMeta
 
+
 class RoomConstraint(object):
     __metaclass__ = ABCMeta
 
-
     @abc.abstractmethod
-    def count_violations(self, state):
+    def count_violations(self, _state):
         return
-    
+
     def is_valid(self, state):
         return 0 == self.count_violations(state)
 
+
 class NullConstraint(RoomConstraint):
 
-    def count_violations(self, state):
+    def count_violations(self, _state):
         return 0
-    
+
 
 class IsolatedItems(RoomConstraint):
-    # TODO: for the meantime assume we have just 1x1 items. How to count arbitrary shapes?
+    # TODO: assume we have just 1xN items. How to count arbitrary shapes?
     def count_violations(self, state):
         count = 0
         seats = state.seats
         for i in xrange (0, len(seats) - 1):
             if seats[i] and seats[i+1]:
                 count += 1
-        
+
         return count
-            
