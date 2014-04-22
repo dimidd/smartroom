1bc10eebceafd2df138b61d0198c9dc8c9188073
Dimid Duchovny <dimidd@NO_SPAM.gmail.com>
2014-04-22 14:02:20 +0300

    item: pep8

--- smartroom/room_item.py
+++ smartroom/room_item.py
@@ -6,32 +6,34 @@ Created on 10/04/2014
 '''
 from abc import ABCMeta
 
+
 class ItemDescriptor(object):
     def __init__(self, item, count):
-        self.item=item
-        self.count=count
+        self.item = item
+        self.count = count
+
 
 class RoomItem(object):
     __metaclass__ = ABCMeta
     '''
     classdocs
     '''
-    
+
     @abc.abstractmethod
     def binary(self):
         '''
-        Return representation of this item as a binary image. i.e a 2d list of booleans
+        Representation of this item as a binary image. i.e a 2d list of bools
         '''
         return
 
-class Bench(RoomItem):    
+
+class Bench(RoomItem):
 
     def __init__(self, n_seats):
         '''
         Constructor
         '''
-        self.ns_seats = n_seats
-        
-    
+        self.n_seats = n_seats
+
     def binary(self):
         return [[True] * self.ns_seats]   
