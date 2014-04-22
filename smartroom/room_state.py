c051923ea0dd3a954d1b91bc4c7ad32c38130e4a
Dimid Duchovny <dimidd@NO_SPAM.gmail.com>
2014-04-22 15:53:32 +0300

    state: Segment

--- smartroom/room_state.py
+++ smartroom/room_state.py
@@ -1,3 +1,20 @@
+import copy
+from room_item import ItemDescriptor, Bench
+from smartroom.room_goal import GoalVerifier
+
+
+class Segment:
+    def __init__(self, lcorner, size):
+        self.lcorner = lcorner
+        self.size = size
+
+    def __str__(self):
+        return '(LC:' + str(self.lcorner) + ', SZ:' + str(self.size) + ')'
+
+    def __repr__(self):
+        return str(self)
+
+
 class RoomState(object):
     def __init__(self, seats, bounds):
         self.seats = seats
