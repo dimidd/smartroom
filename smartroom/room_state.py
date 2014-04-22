19deb97739f6db836fcddce534e6d076cc101d67
Dimid Duchovny <dimidd@NO_SPAM.gmail.com>
2014-04-22 15:56:30 +0300

    state: analyze

--- smartroom/room_state.py
+++ smartroom/room_state.py
@@ -35,3 +35,39 @@ class RoomState(object):
                 res += '|' * self.bounds[i + 1]
                 res += ' '
         return res
+
+    #TODO: save this in state, rather than recomputing?
+    def analyze(self, verf):
+        spaces = []
+        shapes = []
+        unsatisfied = copy.deepcopy(verf.descriptors)
+        seats = self.seats
+        bounds = self.bounds
+        item_sizes = dict()
+        for i, desc in enumerate(unsatisfied):
+            sz = desc.item.size()
+            item_sizes[sz] = i
+
+        i = 0
+        while i < len(seats):
+            if not seats[i]:
+                cur_spaces = []
+                while i < len(seats) and not seats[i]:
+                    cur_spaces.append(Segment(i, -1))
+                    i += 1
+                for seg in cur_spaces:
+                    seg.size = i - seg.lcorner
+                spaces.extend(cur_spaces)
+
+            else:
+                # TODO: check consistency
+                lcorner = i
+                i = i + 1
+                while i < len(seats) and 0 == bounds[i]:
+                    i += 1
+                sz = i - lcorner
+                shapes.append(Segment(lcorner, sz))
+                if sz in item_sizes:
+                    unsatisfied[item_sizes[sz]].count -= 1
+
+        return spaces, shapes, unsatisfied
