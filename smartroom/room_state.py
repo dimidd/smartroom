9fcb534611924e21714481c804e53c45de152bec
Dimid Duchovny <dimidd@NO_SPAM.gmail.com>
2014-05-05 23:23:36 +0300

    state: rename field

--- smartroom/room_state.py
+++ smartroom/room_state.py
@@ -6,7 +6,7 @@ from smartroom.room_goal import GoalVerifier
 class Segment:
     def __init__(self, lcorner, size):
         self.lcorner = lcorner
-        self.size = size
+        self.sz = size
 
     def __str__(self):
         return '(LC:' + str(self.lcorner) + ', SZ:' + str(self.size) + ')'
